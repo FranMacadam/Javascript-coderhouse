cd6b12fbbe6d6bf0a2009982cfc945fbb8f879b9
FranMacadam <87732652+FranMacadam@users.noreply.github.com>
2021-09-14 19:38:34 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,111 @@
-/*!
-* Start Bootstrap - Scrolling Nav v5.0.3 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
 //
 // Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
+//
+window.addEventListener("DOMContentLoaded", (event) => {
+	// Activate Bootstrap scrollspy on the main nav element
+	const mainNav = document.body.querySelector("#mainNav");
+	if (mainNav) {
+		new bootstrap.ScrollSpy(document.body, {
+			target: "#mainNav",
+			offset: 74
+		});
+	}
+	// Collapse responsive navbar when toggler is visible
+	const navbarToggler = document.body.querySelector(".navbar-toggler");
+	const responsiveNavItems = [].slice.call(
+		document.querySelectorAll("#navbarResponsive .nav-link")
+	);
+	responsiveNavItems.map(function (responsiveNavItem) {
+		responsiveNavItem.addEventListener("click", () => {
+			if (window.getComputedStyle(navbarToggler).display !== "none") {
+				navbarToggler.click();
+			}
+		});
+	});
 });
 
-
-
 /*variables*/
-const data =  [
-    {name:"Bitcoin", price: 4500000, img: "Images/Btc.jpg", id: 1},
-    {name:"Ethereum", price: 300000, img: "Images/Eth.jpg", id: 2},
-    {name:"Cardano", price: 225, img: "Images/Ada.jpg", id: 3},
-    {name:"Binance Coin", price: 45000, img:"Images/Bnb.jpg", id: 4},
-  ];
-  let selectedId = null;
-  
-  
-  
-  
-  
-  /*Class*/
-  class Crypto {
-  constructor(title, price, img, id) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = id;
-  }
-  cryptoConverter (insertPesos, equalizer) {
-    let convertCrypto = Number(insertPesos / equalizer);
-    return convertCrypto;
-  }
-  }
-  
+const data = [
+	{ name: "Bitcoin", price: 4500000, img: "/Images/Btc.jpg", id: 1 },
+	{ name: "Ethereum", price: 300000, img: "/Images/Eth.jpg", id: 2 },
+	{ name: "Cardano", price: 225, img: "/Images/Ada.jpg", id: 3 },
+	{ name: "Binance Coin", price: 45000, img: "/Images/Bnb.jpg", id: 4 }
+];
+let acumulator = ``;
+let result = ``;
 
-  let acumulator = ``;
-  data.forEach((Crypto) => {
-    acumulator += `<div class="glowing-border">
-    <div class="card bg-dark optionHide" data-id="${Crypto.id}" style="width: 15rem; height: 25rem;">
-        <img src="${Crypto.img}" class="card-img-top">
-        <div class="card-body text-center pt-5">
-            <p>${Crypto.name}</p><button type="button" class="btn btn-success" id="addToCart">Add to cart</button>
-        </div>
-    </div>
-  </div>`
-  });
-  
-  document.getElementById('cryptoFlexy').innerHTML = acumulator;
-
-
+/*Class*/
+class Crypto {
+	constructor(name, price, img, id) {
+		this.name = name;
+		this.price = price;
+		this.img = img;
+		this.id = id;
+	}
+}
 
 function mostrar(id) {
-    $(".card").hide();
-    $(".card[data-id='"+id+"']").show();
+	$(".card").hide();
+	$(".card[data-id="+id+"]").show();
 }
+
+function calculo() {
+	let pesosInserted = $("#pesosInserted").val();
+	let chosenCrypto = $("#status").val();
+    let cryptoName = $("#status option:selected").text();
+	let cryptoPrice = $(".card[data-id="+chosenCrypto+"]").attr('data-price');
+    let result = pesosInserted / cryptoPrice;
+    console.log(result.toFixed(5));
+    document.getElementById("convertedValue").innerHTML = 'You have to pay '+result.toFixed(5)+' '+cryptoName+' or '+pesosInserted+' pesos.';
+}
+
+
+data.forEach((Crypto) => { 
+    acumulator += `<div class="glowing-border"> <div class="card optionHide" data-id="${Crypto.id}" data-price="${Crypto.price}" style="width: 15rem; height: 25rem;"> <img src="${Crypto.img}" class="card-img-top"> <div class="card-body text-center pt-5"> <p>${Crypto.name}</p><button type="button" class="btn btn-success" onclick="showMsg()">Check value converted</button> </div> </div> </div>`
+});
+
+function showMsg() {
+    let pesosInserted = $("#pesosInserted").val();
+	let chosenCrypto = $("#status").val();
+    let cryptoName = $("#status option:selected").text();
+	let cryptoPrice = $(".card[data-id="+chosenCrypto+"]").attr('data-price');
+    let result = pesosInserted / cryptoPrice;
+    alert('You can afford '+result.toFixed(5)+' '+cryptoName);
+}
+
+
+document.getElementById("cryptoFlexy").innerHTML = acumulator;
+
+function addToCart(){
+    let pesosInserted = $("#pesosInserted").val();
+	let chosenCrypto = $("#status").val();
+    let cryptoName = $("#status option:selected").text();
+	let cryptoPrice = $(".card[data-id="+chosenCrypto+"]").attr('data-price');
+    let result = pesosInserted / cryptoPrice;
+    console.log(JSON.stringify(result));
+}
+
+loaded = true;
+
+window.onload = loaded;
+
+let savedName = localStorage.getItem('reqName');
+if (savedName==null) {
+  let reqName = prompt("Enter your name:");
+  if(reqName){
+    localStorage.setItem('reqName', reqName);
+    alert('Hello '+reqName+'!');
+  }else{
+    alert('I need your name!');
+  }
+}else{
+  alert('Hello '+savedName+'!');
+}
+
+
+
+
+
+
+
 
