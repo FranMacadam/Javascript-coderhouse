d1edb9d805f0c8b7e42589eed1afbd0fcecd07df
FranMacadam <87732652+FranMacadam@users.noreply.github.com>
2021-09-27 17:33:53 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 /*variables*/
+
 const data = [
 	{ name: "Bitcoin", price: 4500000, img: "/Images/Btc.jpg", id: 1 },
 	{ name: "Ethereum", price: 300000, img: "/Images/Eth.jpg", id: 2 },
@@ -37,18 +38,21 @@ let result = ``;
 /*Class*/
 class Crypto {
 	constructor(name, price, img, id) {
-		this.name = name;
-		this.price = price;
-		this.img = img;
+		this.name = data.name;
+		this.price = data.price_change_24h;
+		this.img = data.img;
 		this.id = id;
 	}
 }
 
+
+//Show or hide cards generated with forEach
 function mostrar(id) {
 	$(".card").hide();
-	$(".card[data-id="+id+"]").show();
+	$(".card[data-id="+id+"]").fadeIn();
 }
 
+//Converting value from crypto to pesos/usd
 function calculo() {
 	let pesosInserted = $("#pesosInserted").val();
 	let chosenCrypto = $("#status").val();
@@ -56,14 +60,18 @@ function calculo() {
 	let cryptoPrice = $(".card[data-id="+chosenCrypto+"]").attr('data-price');
     let result = pesosInserted / cryptoPrice;
     console.log(result.toFixed(5));
+	if(result){
     document.getElementById("convertedValue").innerHTML = 'You have to pay '+result.toFixed(5)+' '+cryptoName+' or '+pesosInserted+' pesos.';
+	}
 }
 
-
+//Creating crypto cards
 data.forEach((Crypto) => { 
     acumulator += `<div class="glowing-border"> <div class="card optionHide" data-id="${Crypto.id}" data-price="${Crypto.price}" style="width: 15rem; height: 25rem;"> <img src="${Crypto.img}" class="card-img-top"> <div class="card-body text-center pt-5"> <p>${Crypto.name}</p><button type="button" class="btn btn-success" onclick="showMsg()">Check value converted</button> </div> </div> </div>`
 });
 
+
+//Check the value converted with a button
 function showMsg() {
     let pesosInserted = $("#pesosInserted").val();
 	let chosenCrypto = $("#status").val();
@@ -74,8 +82,10 @@ function showMsg() {
 }
 
 
+//Inject cards
 document.getElementById("cryptoFlexy").innerHTML = acumulator;
 
+//Add final result to cart
 function addToCart(){
     let pesosInserted = $("#pesosInserted").val();
 	let chosenCrypto = $("#status").val();
@@ -85,10 +95,8 @@ function addToCart(){
     console.log(JSON.stringify(result));
 }
 
-loaded = true;
 
-window.onload = loaded;
-
+//Save name to localstorage requested at the beggining
 let savedName = localStorage.getItem('reqName');
 if (savedName==null) {
   let reqName = prompt("Enter your name:");
@@ -103,8 +111,22 @@ if (savedName==null) {
 }
 
 
-$('.fw-bolder').fadeOut(1000).fadeIn(5000).css('color', 'red');
 
+
+
+
+
+
+//Get api
+const getServerStatus = () => {
+	fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+	.then(response => response.json())
+	.then(data => console.log(data));
+};
+
+
+//Animations
+$('.fw-bolder').fadeOut(1000).fadeIn(5000).css('color', 'red');
 $(".titulo").css('color', 'red')
             .slideUp(500)
             .delay(1000)
